da49bb56d97b69a7e39c9680686487243ef5e0b6
harpitha12345 <harpithadevops@gmail.com>
2025-03-10 18:11:13 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
 # Create an S3 Bucket
-resource "aws_s3_bucket" "example_bucket" {
+resource "aws_s3_bucket" "bucket123" {
   bucket = "bucket-harpitha-kfjalsdkfjlas"
 
   # Enable versioning
-  versioning {
-    enabled = true
-  }
+  #versioning {
+   # enabled = true
+  #}
 }
 
